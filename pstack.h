9358aece22bb95de35f91b796e3b6d9c23c45864
kmkangutkar <komalkangutkar96@gmail.com>
2015-09-29 20:43:14 +0530

    added

---
+++ pstack.h
@@ -0,0 +1,14 @@
+#include"istack.h"
+#define PSIZE 16
+typedef struct pstack{
+	istack *arr[PSIZE];
+	int i;	
+}pstack;
+
+void init(pstack *w);
+void push(pstack *w, istack *p);
+istack* pop(pstack *w);
+int empty(pstack *w);
+int full(pstack *w);
+
+
